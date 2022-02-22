df9675f037361ec51c42162fea851bf6909e6423
Sami <sami.t.arar@hotmail.com>
2022-02-21 22:24:13 -0500

    added media

--- src/components/Pages/Realisations.js
+++ src/components/Pages/Realisations.js
@@ -5,18 +5,21 @@ import Image2 from '../../assets/2.jpg'
 import Image3 from '../../assets/3.jpg'
 import Image4 from '../../assets/4.2.jpg'
 import Image5 from '../../assets/5.jpg'
-import Image6 from '../../assets/pexels-francesco-ungaro-6530797.jpg'
+import Image6 from '../../assets/6.jpg'
+import TopImage from '../../assets/pexels-francesco-ungaro-6530797.jpg'
 import Video1 from '../../assets/1.mp4'
 import Video2 from '../../assets/2.mp4'
 import Video3 from '../../assets/3.mp4'
 import Video4 from '../../assets/4.mp4'
+import Video5 from '../../assets/5.mp4'
+import Video6 from '../../assets/6.mp4'
 import { ScrollButton } from "./ScrollButton";
 
 export const Realisations = () => {
   return (
     <>
       <Row id="topimagerow" style={{ postion: "relative" }}>
-        <Image style={{ height: "400px", width: '100%', objectFit: 'cover' }} src={Image6} />
+        <Image style={{ height: "400px", width: '100%', objectFit: 'cover' }} src={TopImage} />
         <ScrollButton />
       </Row>
 
@@ -27,6 +30,9 @@ export const Realisations = () => {
         <Container className="mb-5">
 
           <CardGroup >
+            <Card className="m-3">
+              <Card.Img style={{ height: "100%" }} src={Image6} />
+            </Card>
             <Card className="m-3">
               <Card.Img style={{ height: "100%" }} src={Image4} />
             </Card>
@@ -52,8 +58,10 @@ export const Realisations = () => {
           <h3>VIDEOS</h3>
         </Row>
         <Container className="mb-5">
-
           <CardGroup >
+            <Card className="m-3">
+              <video style={{ objectFit: "fill", height: "100%" }} src={Video5} controls muted type="video/mp4" />
+            </Card>
             <Card className="m-3">
               <video style={{ objectFit: "fill", height: "100%" }} src={Video1} controls muted type="video/mp4" />
             </Card>
@@ -62,8 +70,10 @@ export const Realisations = () => {
             </Card>
           </CardGroup>
 
-
           <CardGroup >
+            <Card className="m-3">
+              <video style={{ objectFit: "fill", height: "100%" }} src={Video6} controls muted type="video/mp4" />
+            </Card>
             <Card className="m-3">
               <video style={{ objectFit: "fill", height: "100%" }} src={Video3} controls muted type="video/mp4" />
             </Card>
